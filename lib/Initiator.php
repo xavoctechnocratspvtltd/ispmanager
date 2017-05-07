b1b654e8d949539e643e71cd3276132db956dec9
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-05-07 17:18:58 +0530

    kB bug fixed

--- lib/Initiator.php
+++ lib/Initiator.php
@@ -26,7 +26,7 @@ class Initiator extends \Controller_Addon {
     function addAppFunctions(){
         
         $this->app->addMethod('byte2human',function($app,$bytes, $decimals = 2){
-            $size = array('B','kB','MB','GB','TB','PB','EB','ZB','YB');
+            $size = array('B','KB','MB','GB','TB','PB','EB','ZB','YB');
             $factor = floor((strlen($bytes) - 1) / 3);
             return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$size[$factor];
         });

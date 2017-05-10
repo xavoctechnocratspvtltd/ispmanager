0d1cac36619c565fc79f2695956a7bf8a8321a5a
monikapokra <monikagupta495@gmail.com>
2017-05-10 16:05:09 +0530

    implementing reset plan and topup

--- lib/Controller/ResetUserPlanAndTopup.php
+++ lib/Controller/ResetUserPlanAndTopup.php
@@ -9,10 +9,33 @@ class Controller_ResetUserPlanAndTopup extends \AbstractController {
 	function run($date = null){
 		if(!$date) $date = $this->app->now;
 
-		// IF TODAY IS RESET DATE
-			//IF DATA IS CARRY FORWARD THEN UPDATE THE DATA LIMIT = (PLAN DATA LIMIT + REMAINING DATA LIMIT OF LAST PERIOD)
-			// RESET TO ZERO OF download_data_consumed AND UPLOAD_data_consumed
-			// UPDATE THE RESET DATE = (PLAN RESET INTERVAL + CONDITION RESET DATE)
+		$upt_model = $this->add('xavoc\ispmanager\Model_UserPlanAndTopup');
+		$upt_model->addCondition('is_expired',false);
+
+
+		foreach ($upt_model as $key => $model) {
+			// IF TODAY IS RESET DATE
+			if($model['reset_date'] <= $date){
+				if($model['is_data_carry_forward']){
+					//IF DATA IS CARRY FORWARD THEN UPDATE THE DATA LIMIT = (PLAN DATA LIMIT + REMAINING DATA LIMIT OF LAST PERIOD)
+				}
+
+				// RESET TO ZERO OF download_data_consumed AND UPLOAD_data_consumed
+				$model['download_data_consumed'] = 0;
+				$model['upload_data_consumed'] = 0;
+
+				// UPDATE THE RESET DATE = (PLAN RESET INTERVAL + CONDITION RESET DATE)
+				$reset_date = date("Y-m-d H:i:s", strtotime("+".$model['data_reset_value']." ".$model['data_reset_mode'],strtotime($model['reset_date'])));
+				$model['reset_date'] = $reset_date;
+
+				// is expire
+				if($model['expire_date'] <= $date)
+					$model['is_expired'] = true;
+
+				$model->saveAndUnload();
+			}
+			
+		}
 		
 		// IF TODAY IS EXPIRE DATE
 			//EXPIRE CONDITION

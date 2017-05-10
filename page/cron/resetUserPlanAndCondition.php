29f8caed370254b642f44210a4c1ac6c5292edf5
sinharakesh <rksinha.btech@gmail.com>
2017-05-10 14:54:51 +0530

    basic login algo

--- page/cron/resetUserPlanAndCondition.php
+++ page/cron/resetUserPlanAndCondition.php
@@ -7,19 +7,17 @@ class page_cron_resetUserPlanAndCondition extends \xepan\base\Page{
 	function run($date = null){
 		if(!$date) $date = $this->app->now;
 
-		/*
-		****
-		implementing plans for 1st month it's invoice is paid 
-		DL = 50GB start date = "20-may-2017" end-date = "20-june-2017" expire date ="25-june-2017"
-		// next month june on date 20-june
-			send email or notify to admin about the user plan workingig in grace period please paid your account or activate
-			// on date 25-june-2017
-			// expire the plan and send email
+		// IF TODAY IS RESET DATE
+			//IF DATA IS CARRY FORWARD THEN UPDATE THE DATA LIMIT = (PLAN DATA LIMIT + REMAINING DATA LIMIT OF LAST PERIOD)
+			// RESET TO ZERO OF download_data_consumed AND UPLOAD_data_consumed
+			// UPDATE THE RESET DATE = (PLAN RESET INTERVAL + CONDITION RESET DATE)
+		
+		// IF TODAY IS EXPIRE DATE
+			//EXPIRE CONDITION
+			//SEND EMAIL OR NOTIFY TO CUSTOMER AND ADMIN
+		// IF TODAY IS END DATE
+			//SEND EMAIL OR NOTIFY TO CUSTOMER AND ADMIN
 
-		actually cron job only set the plan or topis is expired or not
-		reseting the values part of at time of invoice PaidAndActivate function
-		****
-		*/
-
+		
 	}
 }

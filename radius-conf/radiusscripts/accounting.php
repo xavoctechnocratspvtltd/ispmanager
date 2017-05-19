35baa7bb5109f35f0603fdbf1f938b856eab73a5
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-05-19 18:41:09 +0530

    radius code updated

--- radius-conf/radiusscripts/accounting.php
+++ radius-conf/radiusscripts/accounting.php
@@ -2,21 +2,27 @@
 
 include "db.php";
 
-if($_SERVER['ACCT_STATUS_TYPE'] === 'Start') exit(0);
-
-$dl_data = $_SERVER['ACCT_INPUT_OCTETS'];
-$ul_data = $_SERVER['ACCT_OUTPUT_OCTETS'];
-
-$final_row = updateAccountingData($dl_data,$ul_data,$now,$day,$username, $user_data);
-
-if($final_row['Tmp-Integer-0']==='1'){
-	// coa
-	echo "Tmp-Integer-0 := 1";
-	echo "Tmp-String-0 := ".$final_row['ul_limit'].'/'.$final_row['dl_limit'];
-}elseif($final_row['Tmp-Integer-0']==='2'){
-	// disconnect
-	echo "Tmp-Integer-0 := 2";
-}else{
-	// no change
+if(in_array($_SERVER['ACCT_STATUS_TYPE'], ['Start','Accounting-On'])){
 	echo "Tmp-Integer-0 := 0";
+	exit;
+}elseif($_SERVER['ACCT_STATUS_TYPE'] == 'Stop'){
+	$dl_data = $_SERVER['ACCT_INPUT_OCTETS'];
+	$ul_data = $_SERVER['ACCT_OUTPUT_OCTETS'];
+	updateAccountingData($dl_data,$ul_data,$now,$day,$username, $user_data);
+	echo "Tmp-Integer-0 := 0";
+	exit;
+}else{
+	// it's interim
+	$final_row = checkCOA($now,$day,$username, $user_data);
+	if($final_row['Tmp-Integer-0']===1){
+		// coa
+		echo "Tmp-Integer-0 := 1,";
+		echo "Tmp-String-0 := ".$final_row['ul_limit'].'/'.$final_row['dl_limit'];
+	}elseif($final_row['Tmp-Integer-0']===2){
+		// disconnect
+		echo "Tmp-Integer-0 := 2";
+	}else{
+		// no change
+		echo "Tmp-Integer-0 := 0";
+	}
 }
--- radius-conf/radiusscripts/auth.php
+++ radius-conf/radiusscripts/auth.php
@@ -3,7 +3,7 @@
 include "db.php";
 
 $final_row = checkAuthentication($now,$day,$username, $user_data);
-if(!$result['access']) exit(1);
+if(!$final_row['access']) exit(1);
 if($final_row['dl_limit']){
 	$ul_limit = $final_row['ul_limit'];
 	$dl_limit = $final_row['dl_limit'];

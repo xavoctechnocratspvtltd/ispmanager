928b2519db34c429f9b548c85c252620ccce8858
vijay mali <guddavijay@gmail.com>
2017-06-17 15:43:18 +0530

    hotspot registration & verify customer with plan done send sms pending

--- page/configuration.php
+++ page/configuration.php
@@ -26,5 +26,58 @@ class page_configuration extends \xepan\base\Page {
 
 		$crud = $city_tab->add('xepan\hr\CRUD');
 		$crud->setModel('xavoc\ispmanager\City');
+
+
+
+
+		// Send OTP SMS for Registar user
+		$sms_model = $this->add('xepan\base\Model_ConfigJsonModel',
+			[
+				'fields'=>[
+							'otp_msg_content'=>'Text',
+						],
+					'config_key'=>'ISPMANAGER_OTP_CONTENT',
+					'application'=>'ispmanager'
+			]);
+		$sms_model->add('xepan\hr\Controller_ACL');
+		$sms_model->tryLoadAny();
+
+		$sms_tab = $tab->addTab('SMS Content');
+		$form = $sms_tab->add('Form');
+		$form->setModel($sms_model);
+		$form->addSubmit('Save');
+		if($form->isSubmitted()){
+			$form->update();
+			$form->js(null,$form->js()->reload())->univ()->successMessage('Saved')->execute();
+		}
+
+		// Default HotSpot Plan 
+		$plan_tab = $tab->addTab('Default HotSpot Plan');
+		$defalut_plan_model = $this->add('xepan\base\Model_ConfigJsonModel',
+			[
+				'fields'=>[
+							'default_hotspot_plan'=>'DropDown',
+						],
+					'config_key'=>'ISPMANAGER_DEFAULT_HOTSPOT_PLAN',
+					'application'=>'ispmanager'
+			]);
+		$defalut_plan_model->add('xepan\hr\Controller_ACL');
+		$defalut_plan_model->tryLoadAny();
+
+		$plan_m = $this->add('xavoc\ispmanager\Model_Plan');
+		if($defalut_plan_model->loaded()){
+			$plan_m->addCondition('id',$defalut_plan_model['default_hotspot_plan']);
+			$plan_m->tryLoadAny();		
+		}
+		$form = $plan_tab->add('Form');
+		$form->setModel($defalut_plan_model);
+
+		$default_plan_field = $form->getElement('default_hotspot_plan');
+		$default_plan_field->setModel($plan_m);
+		$form->addSubmit('Save');
+		if($form->isSubmitted()){
+			$form->update();
+			$form->js(null,$form->js()->reload())->univ()->successMessage('Saved')->execute();
+		}
 	}
 }

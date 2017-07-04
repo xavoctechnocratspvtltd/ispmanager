bc55e6e74438f25f1e19ef056eb80d7310b253c7
vijay mali <guddavijay@gmail.com>
2017-07-04 17:48:02 +0530

    customer registration done on staff panel frontend

--- lib/Tool/CustomerRegistration.php
+++ lib/Tool/CustomerRegistration.php
@@ -15,10 +15,11 @@ class Tool_CustomerRegistration extends \xepan\cms\View_Tool{
 		$form = $this->add('Form');
 		$form->setLayout(['form/staff/userregistration']);
 		$form->setModel($user_model,['plan_id','plan','radius_username','radius_password','simultaneous_use','grace_period_in_days','custom_radius_attributes','mac_address','first_name','last_name','country_id','state_id','city','address','pin_code','organization','billing_address','billing_city','billing_pincode']);
-		$form->addField('dob');
+		$form->addField('DatePicker','dob');
 		$form->addField('pan_number');
 		$form->addField('xepan\base\DropDownNormal','status_of_organization')->setValueList(['Partership_Firm'=>'Partership Firm','Public_Limited_Co.'=>'Public Limited Co','Education_institute'=>'Education institute','Private_limited_company'=>'Private Limited Company','Trust'=>'Trust','Goverment_Organization'=>'Goverment Organization','Other'=>'Other,Please Specify']);
 		$form->addField('xepan\base\DropDownNormal','customer_cat')->setValueList(['Name_Account'=>'Name Account','sme_smb'=>'SME / SMB','SOHO'=>'SOHO','Residential'=>'Residential','ISP'=>'ISP (Please attach a copy of ISP liense)','PCOs'=>'PCOs','OSP_BPO'=>'OSP / BPO (Please attach a copy of ISP liense given by DOT)']);
+		$form->addField('Radio','static_ip')->setValueList(['Yes'=>'Yes','No'=>'No'])->addClass('ispmanager-radio_btn');
 		// $form->addField('general_contact');
 		// $form->addField('technical_contact');
 		// $form->addField('finance_contact');
@@ -35,11 +36,35 @@ class Tool_CustomerRegistration extends \xepan\cms\View_Tool{
 		// $form->addField('make');
 		// $form->addField('mode');
 		// $form->addField('serial');
-		$form->addField('Radio','static_ip')->setValueList(['Yes'=>'Yes','No'=>'No'])->addClass('ispmanager-radio_btn');
 		$form->addSubmit("Submit")->addClass('btn btn-success');
 		
 		if($form->isSubmitted()){
 			$form->update();
+			// $this->add('xepan\base\Model_Contact_Email')
+			// 		->addCondition('contact_id',$user->id)
+			// 		->addCondition('head','Official')
+			// 		->addCondition('value',$form['email'])
+			// 		->addCondition('is_active',true)
+			// 		->addCondition('is_valid',1)
+			// 		->tryLoadAny()
+			// 		->save();
+
+			// // $user->addEmail($form['email']);
+			// $this->add('xepan\base\Model_Contact_Phone')
+			// 		->addCondition('contact_id',$user->id)
+			// 		->addCondition('head','Official')
+			// 		->addCondition('value',$form['contact'])
+			// 		->addCondition('is_active',true)
+			// 		->addCondition('is_valid',1)
+			// 		->tryLoadAny()
+			// 		->save();
+			// $user->addPhone($form['contact']);
+			$this->add('xepan\base\Model_Contact_Event')
+					->addCondition('contact_id',$form->model->id)
+					->addCondition('head',"DOB")
+					->addCondition('value',$form['dob'])
+					->tryLoadAny()
+					->save();
 			$form->js(null,$form->js()->reload())->univ()->successMessage('user created');
 		}
 	}

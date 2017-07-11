18b770a6ec15fa50d32fadc038bf5d5d07fa13d4
sinharakesh <rksinha.btech@gmail.com>
2017-07-11 15:20:59 +0530

    staff panel some html are corrected

--- lib/Tool/User/DashBoard.php
+++ lib/Tool/User/DashBoard.php
@@ -58,9 +58,9 @@ class Tool_User_DashBoard extends \xepan\cms\View_Tool{
 		$up_t_consumed = 0;
 		$total_data_limit = 0;
 		foreach ($list as $key => $condition) {
-			$up_t_consumed += $condition['upload_data_consumed'];
-			$dw_t_consumed += $condition['download_data_consumed'];
-			$total_data_limit += $condition['data_limit'];
+			// $up_t_consumed += $condition['upload_data_consumed'];
+			// $dw_t_consumed += $condition['download_data_consumed'];
+			// $total_data_limit += $condition['data_limit'];
 		}
 
 		$this->template->trySet('consume_data',$user->byte2human($up_t_consumed + $dw_t_consumed));

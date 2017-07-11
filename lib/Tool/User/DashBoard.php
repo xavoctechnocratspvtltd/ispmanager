6850065ee7b4bd2ca028df14c3a2abfba326bec9
sinharakesh <rksinha.btech@gmail.com>
2017-07-11 13:37:49 +0530

    user dashbaord data consumed and total added

--- lib/Tool/User/DashBoard.php
+++ lib/Tool/User/DashBoard.php
@@ -46,12 +46,25 @@ class Tool_User_DashBoard extends \xepan\cms\View_Tool{
 		}
 
 		// echo "string". $user['plan_id'];
-		$user = $this->app->auth->model;
+		// $user = $this->app->auth->model;
 
 
 
 		$this->template->set('plan',$plan['name']);
-		$this->template->set('username',$user['username']);
+		$this->template->set('username',$this->app->auth->model['username']);
+
+		$list = $user->getCurrentCondition();
+		$dw_t_consumed = 0;
+		$up_t_consumed = 0;
+		$total_data_limit = 0;
+		foreach ($list as $key => $condition) {
+			$up_t_consumed += $condition['upload_data_consumed'];
+			$dw_t_consumed += $condition['download_data_consumed'];
+			$total_data_limit += $condition['data_limit'];
+		}
+
+		$this->template->trySet('consume_data',$user->byte2human($up_t_consumed + $dw_t_consumed));
+		$this->template->trySet('total_data_limit',$user->byte2human($total_data_limit));
 
 	}
 

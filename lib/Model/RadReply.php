9af680ed1cd708d5f7c6e1a1e00103b1d83a5065
sinharakesh <rksinha.btech@gmail.com>
2018-04-09 18:41:46 +0530

    static ip unique value checked

--- lib/Model/RadReply.php
+++ lib/Model/RadReply.php
@@ -12,5 +12,25 @@ class Model_RadReply extends \xepan\base\Model_Table{
 		$this->addField('attribute');
 		$this->addField('op');
 		$this->addField('value');
+
+		$this->addHook('beforeSave',$this);
+	}
+
+	function beforeSave(){
+		$this['value'] = trim($this['value']);
+		
+		$old_model = $this->add('xavoc\ispmanager\Model_RadReply');
+		$old_model->addCondition('username',$this['username']);
+		$old_model->addCondition('op',$this['op']);
+		$old_model->addCondition('attribute',$this['attribute']);
+		$old_model->addCondition('value',$this['value']);
+		$old_model->addCondition("id",'<>',$this->id);
+		$old_model->tryLoadAny();
+
+		if($old_model->loaded())
+			throw $this->exception('Value is already exist', 'ValidityCheck')
+                        ->setField('value');
+			
+
 	}
 }

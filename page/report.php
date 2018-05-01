89cfca4a0660f6fef8ea39361db8a32e22ea8e1e
sinharakesh <rksinha.btech@gmail.com>
2018-05-01 15:37:59 +0530

    page report user under fup on today added

--- page/report.php
+++ page/report.php
@@ -5,9 +5,25 @@ namespace xavoc\ispmanager;
 class page_report extends \xepan\base\Page {
 	
 	public $title ="Reports";
-	function init(){
-		parent::init();
 
+	function page_index(){
+		$tab = $this->add('Tabs');
+		$tab->addTabUrl('./user','User');
+	}
+
+	function page_user(){
+
+		$date = $this->app->today;
+		$this->add('View')->set('User Under FUP on date: '.$date);
+
+		$m = $this->add('xavoc\ispmanager\Model_UserPlanAndTopup');
+		$m->addCondition('data_consumed','>=',$m->getElement('net_data_limit'));
+		$m->addCondition('net_data_limit','>',0);
+		$m->addCondition('reset_date',$date);
+
+		$grid = $this->add('xepan\hr\Grid');
+		$grid->setModel($m,['user','plan','remark','reset_date','net_data_limit','data_consumed']);
+		$grid->addPaginator($ipp=50);
 
 	}
 }

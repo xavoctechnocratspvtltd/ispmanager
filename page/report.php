a17c593dd57fe20261d245771304c4f11bfd7877
sinharakesh <rksinha.btech@gmail.com>
2018-05-01 15:53:34 +0530

    non expired condition show only

--- page/report.php
+++ page/report.php
@@ -17,12 +17,14 @@ class page_report extends \xepan\base\Page {
 		$this->add('View')->set('User Under FUP on date: '.$date);
 
 		$m = $this->add('xavoc\ispmanager\Model_UserPlanAndTopup');
+		$m->addExpression('user_status')->set($m->refSql('user_id')->fieldQuery('status'));
 		$m->addCondition('data_consumed','>=',$m->getElement('net_data_limit'));
 		$m->addCondition('net_data_limit','>',0);
 		$m->addCondition('reset_date',$date);
+		$m->addCondition([['is_expired',false],['is_expired',null]]);
 
 		$grid = $this->add('xepan\hr\Grid');
-		$grid->setModel($m,['user','plan','remark','reset_date','net_data_limit','data_consumed']);
+		$grid->setModel($m,['user','plan','remark','reset_date','net_data_limit','data_consumed','user_status']);
 		$grid->addPaginator($ipp=50);
 
 	}

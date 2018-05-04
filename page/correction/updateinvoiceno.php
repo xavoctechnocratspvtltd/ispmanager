ea3a8c6c58b5bbfb6f9ad9abddd1e4c720adc4a4
sinharakesh <rksinha.btech@gmail.com>
2018-05-04 11:15:17 +0530

    invoice no reset corrected

--- page/correction/updateinvoiceno.php
+++ page/correction/updateinvoiceno.php
@@ -15,11 +15,22 @@ class page_correction_updateinvoiceno extends \xepan\base\Page {
 		if($_GET['do']){
 			$i = 1;
 			foreach ($inv as $model) {
-				if($i == 16) $i = 20;
-				if($i == 22) $i = 23;
-				if($i == 24) $i = 26;
-				if($i == 24) $i = 26;
-				if($i == 131) $i = 139;
+				
+				if($i == 7) $i = 8;
+				if($i == 10) $i = 11;
+				if($i == 16) $i = 20; // 16 to 19
+				if($i == 22) $i = 23; // 21, 22
+				if($i == 24) $i = 26; // 24, 25 
+				if($i == 27) $i = 28;
+				if($i == 39) $i = 40;
+				if($i == 43) $i = 44;
+				if($i == 48) $i = 49; 
+				if($i == 79) $i = 80;
+				if($i == 89) $i = 90; 
+				if($i == 131) $i = 139; // 131 to 138
+				if($i == 380) $i = 382; // 380, 381
+				if($i == 359) $i = 361; // 359, 360
+				if($i == 421) $i = 431; // 421 to 430
 
 				$last_inv = $model['document_no'];
 				$model['document_no'] = $i;

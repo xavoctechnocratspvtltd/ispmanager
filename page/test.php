de800aef238ce70e3d42774678b9bfac0063ae18
sinharakesh <rksinha.btech@gmail.com>
2019-02-28 11:46:15 +0530

    test all test code removed

--- page/test.php
+++ page/test.php
@@ -12,9 +12,9 @@ class page_test extends \xepan\base\Page {
 		// $user_name = 'vivekjain';
 		// $start_date = '2018-08-01';
 		// $end_date = '2018-09-01';
-		$user_name = 'oscarspvtltd';
-		$start_date = '2018-08-05';
-		$end_date = '2018-09-05';
+		// $user_name = 'oscarspvtltd';
+		// $start_date = '2018-08-05';
+		// $end_date = '2018-09-05';
 
 
 		// $user_name = 'virendrakumarjain';
@@ -29,51 +29,51 @@ class page_test extends \xepan\base\Page {
 		// $start_date = '2018-07-21';
 		// $end_date = '2018-08-24';
 
-		$radacct = $this->add('xavoc\ispmanager\Model_RadAcct');
-		$radacct->addCondition('username',$user_name);
-		$radacct->addCondition('acctstarttime','>=',$start_date);
-		$radacct->addCondition('acctstarttime','<',$end_date);
-		$radacct->addCondition('acctstoptime','<>',NULL);
+		// $radacct = $this->add('xavoc\ispmanager\Model_RadAcct');
+		// $radacct->addCondition('username',$user_name);
+		// $radacct->addCondition('acctstarttime','>=',$start_date);
+		// $radacct->addCondition('acctstarttime','<',$end_date);
+		// $radacct->addCondition('acctstoptime','<>',NULL);
 
-		$act_1 = $radacct->sum('acctinputoctets')->getOne();
-		$act_2 = $radacct->sum('acctoutputoctets')->getOne();
+		// $act_1 = $radacct->sum('acctinputoctets')->getOne();
+		// $act_2 = $radacct->sum('acctoutputoctets')->getOne();
 
-		$col = $this->add('Columns');
-		$col1 = $col->addColumn('4');
-		$col2 = $col->addColumn('4');
-		$col3 = $col->addColumn('4');
+		// $col = $this->add('Columns');
+		// $col1 = $col->addColumn('4');
+		// $col2 = $col->addColumn('4');
+		// $col3 = $col->addColumn('4');
 
-		$col1->add('View')->set("Closed Radius Session");
-		$col1->add('View')->set($this->app->byte2human($act_1));
-		$col1->add('View')->set($this->app->byte2human($act_2));
-		$col1->add('View')->setHtml("<hr>Total: ".$this->app->byte2human($act_2 + $act_1));
-		$col1->add('View')->setElement('hr');
+		// $col1->add('View')->set("Closed Radius Session");
+		// $col1->add('View')->set($this->app->byte2human($act_1));
+		// $col1->add('View')->set($this->app->byte2human($act_2));
+		// $col1->add('View')->setHtml("<hr>Total: ".$this->app->byte2human($act_2 + $act_1));
+		// $col1->add('View')->setElement('hr');
 
 
-		$model = $this->add('xavoc\ispmanager\Model_UserPlanAndTopup');
-		$model->addExpression('usernameradius')->set($model->refSQL('user_id')->fieldQuery('radius_username'));
-		$model->addCondition('usernameradius',$user_name);
-		$model->addCondition('is_effective',true);
-		$model->setOrder('id','desc');
+		// $model = $this->add('xavoc\ispmanager\Model_UserPlanAndTopup');
+		// $model->addExpression('usernameradius')->set($model->refSQL('user_id')->fieldQuery('radius_username'));
+		// $model->addCondition('usernameradius',$user_name);
+		// $model->addCondition('is_effective',true);
+		// $model->setOrder('id','desc');
 
-		$this->add('View')->set("User Condition data");
-		$this->add('Grid')->setModel($model,['session_download_data_consumed','session_upload_data_consumed','session_download_data_consumed_on_reset','session_upload_data_consumed_on_reset','download_data_consumed','upload_data_consumed','data_consumed']);
+		// $this->add('View')->set("User Condition data");
+		// $this->add('Grid')->setModel($model,['session_download_data_consumed','session_upload_data_consumed','session_download_data_consumed_on_reset','session_upload_data_consumed_on_reset','download_data_consumed','upload_data_consumed','data_consumed']);
 		
-		$acctmodel = $this->add('xavoc\ispmanager\Model_RadAcct');
-		$acctmodel->addCondition('username',$user_name);
-		$acctmodel->addCondition('acctstarttime','>=',$start_date);
-		$acctmodel->addCondition('acctstarttime','<=',$end_date);
-		$acctmodel->addCondition('acctstoptime',NULL);
+		// $acctmodel = $this->add('xavoc\ispmanager\Model_RadAcct');
+		// $acctmodel->addCondition('username',$user_name);
+		// $acctmodel->addCondition('acctstarttime','>=',$start_date);
+		// $acctmodel->addCondition('acctstarttime','<=',$end_date);
+		// $acctmodel->addCondition('acctstoptime',NULL);
 
-		$act_11 = $acctmodel->sum('acctinputoctets')->getOne();
-		$act_12 = $acctmodel->sum('acctoutputoctets')->getOne();
+		// $act_11 = $acctmodel->sum('acctinputoctets')->getOne();
+		// $act_12 = $acctmodel->sum('acctoutputoctets')->getOne();
 
-		$col2->add('View')->set("Un Closed Radius Session");
-		$col2->add('View')->set($this->app->byte2human($act_11));
-		$col2->add('View')->set($this->app->byte2human($act_12));
-		$col2->add('View')->setHtml('<hr> Total: '.$this->app->byte2human($act_12+$act_11));
+		// $col2->add('View')->set("Un Closed Radius Session");
+		// $col2->add('View')->set($this->app->byte2human($act_11));
+		// $col2->add('View')->set($this->app->byte2human($act_12));
+		// $col2->add('View')->setHtml('<hr> Total: '.$this->app->byte2human($act_12+$act_11));
 
-		$col3->add('View')->setHtml("Total: ".$this->app->byte2human($act_12+$act_11+$act_1+$act_2));
+		// $col3->add('View')->setHtml("Total: ".$this->app->byte2human($act_12+$act_11+$act_1+$act_2));
 		// if($_GET['update_invoice']){
 		// 	$m = $this->add('xepan\commerce\Model_SalesInvoice');
 		// 	foreach ($m as $temp) {
@@ -151,10 +151,11 @@ class page_test extends \xepan\base\Page {
 		// 	$form->js()->univ()->successMessage('saved')->execute();
 		// }
 		
-		// if($_GET['reset']){
-		// 	$date = $_GET['date']?:$this->app->today;
-		// 	$this->add('xavoc\ispmanager\Controller_ResetUserPlanAndTopup')->run($date);
-		// }
+		if($_GET['reset']){
+			$date = $_GET['date']?:$this->app->today;
+			$date = '2018-12-5';
+			$this->add('xavoc\ispmanager\Controller_ResetUserPlanAndTopup')->run($date);
+		}
 
 	}
 }

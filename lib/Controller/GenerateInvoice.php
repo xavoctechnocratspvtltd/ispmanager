e46b064067ff7a15d5dced2282e06edb2abc8efe
sinharakesh <rksinha.btech@gmail.com>
2019-04-08 11:12:13 +0530

    not generating invoice if invoice is created in same month of plan validity

--- lib/Controller/GenerateInvoice.php
+++ lib/Controller/GenerateInvoice.php
@@ -20,6 +20,8 @@ class Controller_GenerateInvoice extends \AbstractController {
 		$model->addExpression('user_status')->set(function($m,$q){
 			return $m->refSQL("user_id")->fieldQuery('status');
 		});
+		$model->addExpression('plan_validity_value')->set($model->refSQL('plan_id')->fieldQuery('plan_validity_value'));
+		$model->addExpression('qty_unit')->set($model->refSQL('plan_id')->fieldQuery('qty_unit'));
 
 		if($to_date)
 			$model->addCondition('end_date','<=',$to_date);
@@ -29,16 +31,20 @@ class Controller_GenerateInvoice extends \AbstractController {
 			$model->addCondition('branch_id',$branch_id);
 
 		$model->addCondition('end_date','<>',$model->getElement('last_invoice_date'));
-		$model->addCondition([['is_expired',false],['is_expired',null]]);
+		// $model->addCondition([['is_expired',false],['is_expired',null]]);
 		$model->addCondition('user_status','Active');
 		$model->_dsql()->where('id in ( select max(id) from isp_user_plan_and_topup group by user_id)');
-		$model->setActualFields(['id','last_invoice_date','end_date','user_status']);
+		$model->setActualFields(['id','last_invoice_date','end_date','user_status','plan_id','plan_validity_value','qty_unit']);
 		
+
 		foreach ($model as $data) {
 			if(strtotime($data['end_date']) == strtotime(date('Y-m-d',strtotime($data['last_invoice_date'])))){
           		continue;
           	}
-
+			
+          	if(strtotime(date('Y-m-d',strtotime($data['last_invoice_date']))) >  strtotime("-".$data['plan_validity_value']." ".$data['qty_unit'],strtotime($data['end_date'])) ) // invoice is created between end_date and plan validity in past .. may be for this renew
+				continue;
+			
           	try{
           		$this->app->db->beginTransaction();
           		// echo "<pre>";
